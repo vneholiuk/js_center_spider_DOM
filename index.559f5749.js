ac908e120d17a685fc370e2e337799708531c71d
Vlados <vnegoluk@gmail.com>
2024-08-05 18:19:42 +0200

    make build

---
+++ index.559f5749.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),l=t.clientWidth,i=t.clientHeight,o=e.offsetWidth,c=e.offsetHeight;e.style.left=l/2-o/2+"px",e.style.top=i/2-c/2+"px";
+//# sourceMappingURL=index.559f5749.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.offsetWidth;
+const spiderHeight = spider.offsetHeight;
+spider.style.left = wallWidth / 2 - spiderWidth / 2 + "px";
+spider.style.top = wallHeight / 2 - spiderHeight / 2 + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
